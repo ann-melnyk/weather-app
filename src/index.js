8dc1928bd82a2b21276dd6df51f1e7a3cea7de32
Anna-mel <127248642+Anna-mel@users.noreply.github.com>
2023-03-13 23:09:23 +0200

    Added relevant current weather icon from API

--- src/index.js
+++ src/index.js
@@ -36,7 +36,12 @@ const displayWeather = (response) => {
   document.querySelector("#wind-speed").innerHTML = Math.round(
     response.data.wind.speed
   );
-  console.log(response);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 };
 const searchCity = (city) => {
   let units = "metric";
